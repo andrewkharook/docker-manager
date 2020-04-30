ea70c371d83f0b58d51c1704b2513ca279d2748a
Andrew Kharook <andriy.kharuk@gmail.com>
2020-04-30 19:50:32 +0300

    Bump application version

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject docker-controller "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject docker-controller "0.1.0"
+  :description "An application to programmatically manage docker containers"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
@@ -14,8 +13,8 @@
          :port    3000}
   :profiles
   {:uberjar    {:aot :all}
-   :production {:ring {:open-browser? false,
-                       :stacktraces?  false,
+   :production {:ring {:open-browser? false
+                       :stacktraces?  false
                        :auto-reload?  false}}
    :dev        {:dependencies [[javax.servlet/servlet-api "2.5"]
                                [ring/ring-mock "0.3.2"]]}}
