c9297efad305f323a5b8aeba02ce6165c86f67e8
Andrew Kharook <andriy.kharuk@gmail.com>
2021-01-20 15:50:08 +0200

    v1.0.0: Update the response format

--- src/docker_controller/routes/service.clj
+++ src/docker_controller/routes/service.clj
@@ -23,3 +23,13 @@
       :body   (json/write-str {:errors [{:status (str status)
                                          :detail text}]})}
      (throw (IllegalArgumentException. "Status code must be an integer.")))))
+
+(defn format-response
+  [response]
+  (assoc response
+    :status (if (< (:status response) 400) 200 (:status response))
+    :headers {"content-type" "application/json"}
+    :body (json/write-str
+            (if (clojure.string/blank? (:body response))
+              "OK"
+              (json/read-str (:body response) :key-fn keyword)))))
